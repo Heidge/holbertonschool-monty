ecba2c96f66d72e25f7720884a51fe20a88478bc
Hedgebzh <axel.gore35@gmail.com>
2022-08-16 12:04:23 +0200

    fix main code

--- push.c
+++ push.c
@@ -1,17 +1,12 @@
 #include "monty.h"
-#include <stdarg.h>
-#include <stdlib.h>
-#include <unistd.h>
-#include <stdio.h>
 
-typedef struct stack_s
+typedef struct instruction_s
 {
-        int n;
-        struct stack_s *prev;
-        struct stack_s *next;
-} stack_t;
+        char *opcode;
+        void (*f)(stack_t **stack, unsigned int line_number);
+} instruction_t;
 
-void empiler(stack_t **p_pile, int n)
+void empiler(stack_t **stack, unsigned line_number)
 {
 	stack_t *p_nouveau = malloc(sizeof(*p_nouveau));
 
