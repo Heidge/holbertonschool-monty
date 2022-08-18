08ffd3ac773d6ec0fa6897c6bbef00ab4445ab63
Maykha27 <5144@holbertonschool.com>
2022-08-18 13:43:28 +0100

    test1

--- monty.h
+++ monty.h
@@ -54,9 +54,7 @@ void sub(stack_t **, unsigned int);
 void add(stack_t **, unsigned int);
 void division(stack_t **, unsigned int);
 void mul(stack_t **, unsigned int);
-void mod(stack_t **, unsigned int);
-void pchar(stack_t **, unsigned int);
-void pstr(stack_t **, unsigned int);
+
 
 int _isdigit(int);
 void is_opcode(char *, stack_t **, unsigned int);
