7683648f87b8ce7ea648d45a508b7ff8efefe1ea
Maykha27 <5144@holbertonschool.com>
2022-08-18 11:52:04 +0100

    monty

--- monty.c
+++ monty.c
@@ -1,9 +1,11 @@
 #include "monty.h"
+
+
 /**
  * push - entry point
  * @stack: stack_t variable
  * @number: int variable
- */
+*/
 
 void push(stack_t **stack, unsigned int number)
 {
@@ -29,7 +31,7 @@ void push(stack_t **stack, unsigned int number)
  * pall - entry point
  * @stack: stack_t variable
  * @line_number: unsigned int variable
- */
+*/
 
 void pall(stack_t **stack, unsigned int line_number)
 {
@@ -45,3 +47,298 @@ void pall(stack_t **stack, unsigned int line_number)
 		tmp = tmp->next;
 	}
 }
+
+/**
+ * pint - entry point
+ * @stack: stack_t variable
+ * @line_number: unsigned int variable
+*/
+
+void pint(stack_t **stack, unsigned int line_number)
+{
+
+	if (stack == NULL || *stack == NULL)
+	{
+		fprintf(stderr, "L%d: can't pint, stack empty\n", line_number);
+		exit(EXIT_FAILURE);
+	}
+	printf("%d\n", (*stack)->n);
+}
+
+/**
+ * nop - entry point
+ * @stack: stack_t variable
+ * @line_number: unsigned int variable
+*/
+
+void nop(stack_t **stack, unsigned int line_number)
+{
+	stack = stack;
+	line_number = line_number;
+}
+
+/**
+ * swap - entry point
+ * @stack: stack_t variable
+ * @line_number: unsigned int variable
+*/
+
+void swap(stack_t **stack, unsigned int line_number)
+{
+	stack_t *tmp;
+
+	tmp = *stack;
+	if (tmp == NULL || (tmp->next == NULL && tmp->prev == NULL))
+	{
+		fprintf(stderr, "L%d: can't swap, stack too short\n", line_number);
+		exit(EXIT_FAILURE);
+	}
+
+	tmp->prev = tmp->next;
+	tmp->next->prev = NULL;
+	tmp->next = tmp->next->next;
+	tmp->prev->next = tmp;
+	(*stack) = tmp->prev;
+}
+/**
+ * pop - entry point
+ * @stack: stack_t variable
+ * @line_number: unsigned int variable
+*/
+
+void pop(stack_t **stack, unsigned int line_number)
+{
+	stack_t *tmp;
+
+	tmp = *stack;
+	if ((tmp) == NULL)
+	{
+		fprintf(stderr, "L%d: can't pop an empty stack\n", line_number);
+		exit(EXIT_FAILURE);
+	}
+
+	if (tmp->next == NULL)
+	{
+		(*stack) = NULL;
+		free(tmp);
+	}
+	else
+	{
+		tmp = tmp->next;
+		tmp->prev = NULL;
+	}
+}
+
+/**
+ * add - entry point
+ * @stack: stack_t variable
+ * @line_number: unsigned int variable
+*/
+
+void add(stack_t **stack, unsigned int line_number)
+{
+	unsigned int sum;
+
+	if ((*stack == NULL) || ((*stack)->next == NULL && (*stack)->prev == NULL))
+	{
+		fprintf(stderr, "L%d: can't add, stack too short\n", line_number);
+		exit(EXIT_FAILURE);
+	}
+	sum = (*stack)->n + (*stack)->next->n;
+
+	pop(stack, 0);
+	pop(stack, 0);
+	push(stack, sum);
+}
+
+/**
+ * sub - entry point
+ * @stack: stack_t variable
+ * @line_number: unsigned int variable
+*/
+
+void sub(stack_t **stack, unsigned int line_number)
+{
+	unsigned int res;
+
+	if ((*stack == NULL) || ((*stack)->next == NULL && (*stack)->prev == NULL))
+	{
+		fprintf(stderr, "L%d: can't sub, stack too short\n", line_number);
+		exit(EXIT_FAILURE);
+	}
+	res = (*stack)->next->n - (*stack)->n;
+
+	pop(stack, 0);
+	pop(stack, 0);
+	push(stack, res);
+}
+
+/**
+ * division - entry point
+ * @stack: stack_t variable
+ * @line_number: unsigned int variable
+*/
+
+void division(stack_t **stack, unsigned int line_number)
+{
+	unsigned int divide;
+
+	if ((*stack == NULL) || ((*stack)->next == NULL && (*stack)->prev == NULL))
+	{
+		fprintf(stderr, "L%d: can't div, stack too short\n", line_number);
+		exit(EXIT_FAILURE);
+	}
+	if ((*stack)->n == 0)
+	{
+		fprintf(stderr, "L%d: division by zero\n", line_number);
+		exit(EXIT_FAILURE);
+	}
+	divide = (*stack)->next->n / (*stack)->n;
+
+	pop(stack, 0);
+	pop(stack, 0);
+	push(stack, divide);
+}
+
+/**
+ * mul - entry point
+ * @stack: stack_t variable
+ * @line_number: unsigned int variable
+*/
+
+void mul(stack_t **stack, unsigned int line_number)
+{
+	unsigned int power;
+
+	if ((*stack == NULL) || ((*stack)->next == NULL && (*stack)->prev == NULL))
+	{
+		fprintf(stderr, "L%d: can't mul, stack too short\n", line_number);
+		exit(EXIT_FAILURE);
+	}
+	power = (*stack)->next->n * (*stack)->n;
+
+	pop(stack, 0);
+	pop(stack, 0);
+	push(stack, power);
+}
+
+/**
+ * mod - entry point
+ * @stack: stack_t variable
+ * @line_number: unsigned int variable
+*/
+
+void mod(stack_t **stack, unsigned int line_number)
+{
+	unsigned int module;
+	(void)line_number;
+
+	if ((*stack == NULL) || ((*stack)->next == NULL && (*stack)->prev == NULL))
+	{
+		fprintf(stderr, "L%d: can't mod, stack too short\n", line_number);
+		exit(EXIT_FAILURE);
+	}
+	if ((*stack)->n == 0)
+	{
+		fprintf(stderr, "L%d: division by zero\n", line_number);
+		exit(EXIT_FAILURE);
+	}
+	module = (*stack)->next->n % (*stack)->n;
+
+	pop(stack, 0);
+	pop(stack, 0);
+	push(stack, module);
+}
+
+/**
+ * pchar - entry point
+ * @stack: stack_t variable
+ * @line_number: unsigned int variable
+*/
+
+void pchar(stack_t **stack, unsigned int line_number)
+{
+	int letter;
+
+	if (*stack == NULL)
+	{
+		fprintf(stderr, "L%d: can't pchar, stack empty\n", line_number);
+		exit(EXIT_FAILURE);
+	}
+	letter = (*stack)->n;
+
+	if (letter >= 0 && letter <= 255)
+	{
+		putchar(letter);
+		putchar('\n');
+	}
+	else
+	{
+		fprintf(stderr, "L%d: can't pchar, value out of range,\n", line_number);
+		exit(EXIT_FAILURE);
+	}
+}
+
+/**
+ * pstr - entry point
+ * @stack: stack_t variable
+ * @line_number: unsigned int variable
+*/
+
+void pstr(stack_t **stack, unsigned int line_number)
+{
+	int string;
+	stack_t *tmp;
+
+	if (stack == NULL)
+	{
+		fprintf(stderr, "L%d: can't pstr, stack empty\n", line_number);
+		exit(EXIT_FAILURE);
+	}
+
+	tmp = *stack;
+	while (tmp)
+	{
+		string = tmp->n;
+		if (string > 0 && string <= 127)
+			putchar(string);
+		else
+			break;
+		tmp = tmp->next;
+	}
+	putchar('\n');
+}
+/**
+ * freestack - enrty point
+ * @stack: stack_t variable
+*/
+
+void freestack(stack_t **stack)
+{
+	stack_t *tmp;
+
+	while ((*stack) != NULL)
+	{
+		tmp = (*stack);
+		(*stack) = (*stack)->next;
+		free(tmp);
+	}
+}
+
+/**
+ * _isdigit - function that checks for a digit (0 through 9)..
+ * @c: digit
+ * Return:Returns 1 when c is a digit and 0 otherwise
+ */
+
+int _isdigit(int c)
+{
+	if (c >= 48 && c <= 57)
+	{
+		return (1);
+	}
+	return (0);
+}
+
+
+
--- monty.h
+++ monty.h
@@ -9,10 +9,6 @@
 #include <stdio.h>
 #include <string.h>
 
-#define UNUSED(x) (void)(x)
-#define TRUE 1
-#define FALSE 0
-#define DELIMS "\n \t\r"
 /**
  * struct stack_s - doubly linked list representation of a stack (or queue)
  * @n: integer
@@ -20,28 +16,48 @@
  * @next: points to the next element of the stack (or queue)
  *
  * Description: doubly linked list node structure
- * for stack, queues, LIFO, FIFO
+ * for stack, queues, LIFO, FIFO Holberton project
  */
 typedef struct stack_s
 {
-        int n;
-        struct stack_s *prev;
-        struct stack_s *next;
+	int n;
+	struct stack_s *prev;
+	struct stack_s *next;
 } stack_t;
+
 /**
  * struct instruction_s - opcode and its function
  * @opcode: the opcode
  * @f: function to handle the opcode
  *
  * Description: opcode and its function
- * for stack, queues, LIFO, FIFO
+ * for stack, queues, LIFO, FIFO Holberton project
  */
 typedef struct instruction_s
 {
-        char *opcode;
-        void (*f)(stack_t **stack, unsigned int line_number);
+	char *opcode;
+	void (*f)(stack_t **stack, unsigned int line_number);
 } instruction_t;
-void push(stack_t **stack, unsigned int number);
-void pall(stack_t **stack, unsigned int line_number);
+
+void push(stack_t **, unsigned int);
+void pop(stack_t **, unsigned int);
+void swap(stack_t **, unsigned int);
+void pint(stack_t **, unsigned int);
+void add(stack_t **, unsigned int);
+void nop(stack_t **, unsigned int);
+void pall(stack_t **, unsigned int);
+void sub(stack_t **, unsigned int);
+void add(stack_t **, unsigned int);
+void division(stack_t **, unsigned int);
+void mul(stack_t **, unsigned int);
+void mod(stack_t **, unsigned int);
+void pchar(stack_t **, unsigned int);
+void pstr(stack_t **, unsigned int);
+
+int _isdigit(int);
+void is_opcode(char *, stack_t **, unsigned int);
+char **parse(char *);
 void freestack(stack_t **);
+void check_push(stack_t **, char **, unsigned int);
+
 #endif
