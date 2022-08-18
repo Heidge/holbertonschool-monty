92fbff7dcb7848a070eae75272eec06bd9f137f1
Hedgebzh <axel.gore35@gmail.com>
2022-08-18 15:32:21 +0200

    fix errors

--- main.c
+++ main.c
@@ -5,12 +5,12 @@ int main (int argc, char *argv[])
 	FILE *fp;
 
 	if (argc != 2)
-	usage_error(); /* appel de la fonction usage_error si utilisateur n'a pas rentré exactement deux arguments **/
+	errors(1, " ", 1);
 
 	fp = fopen(argv[1], "r"); /** "r" pour read -> mode d'utilisation - Ici on veut lire le contenu du fichier .m */
 
 	if (fp == NULL)
-	open_file_failure();
+	errors(2, argv[1], 1);
 
 	monty_interpreter(fp); /**fonction qui permet de sélectionner la bonne fonction le contenu du fichier */
 
--- usage_error.c
+++ usage_error.c
@@ -5,6 +5,8 @@
  */
 void usage_error(void)
 {
+	void(file)
 	fprintf(stderr, "USAGE: monty file\n");
 	exit(EXIT_FAILURE);
+	break;
 }
